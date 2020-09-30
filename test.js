00fcb4bc0a147be28cbe8cc5c217274b8c1107e1
Pepperoni <pepperonicorn@gmail.com>
2020-09-30 14:33:55 +0800

    Remove redundancy with noLinkAlert() function

--- test.js
+++ test.js
@@ -1,30 +1,36 @@
-// Write - Your - Code - Here
+//Create Link class to organize links to add 
 class Link {
-    constructor(title, url, author){//TIL: you can use conditionals in the constructor
+    constructor(title, url, author){
         let absoluteURL = url;
-        if (!url.startsWith("http://") && !url.startsWith("https://")){//prefix
+        //Use conditionals in the constructor to filter url
+        if (!url.startsWith("http://") && !url.startsWith("https://")){
+            //Use template literal to prefix the url
             absoluteURL = `https://${url}`;
         }
         this.title = title;
         this.url = absoluteURL;
         this.author = author;
     }
-
+    //Add a method that returns a string of Link info
     toString(){
         return `${this.title} (${this.url}). Author: ${this.author}`;
     }
 }
 
+//initialize links array
 const links = [];
 links.push(new Link("Hacker News", "https://news.ycombinator.com", "Baptiste"));
 links.push(new Link("Reddit", "https://reddit.com", "Thomas"));
 links.push(new Link("Boing Boing", "boingboing.net", "Daniel"));
 
+
 let choice;
 while(choice !== "0"){
-    const choices = "1: Show links\n2: Add a link\n3: Remove a link\n0:Quit";
-    choice = prompt(`Choose an option: \n${choices}`);
+    //present the user with options
+    const options = "1: Show links\n2: Add a link\n3: Remove a link\n0:Quit";
+    choice = prompt(`Choose an option: \n${options}`);
     
+
     const listLinks = (whatToDo) => {
 		let showLinks = "";
 		let deleteNum;
@@ -33,28 +39,33 @@ while(choice !== "0"){
 				showLinks += `${i + 1}: ` + links[i].toString() + "\n";
 			}
 		}
+
+        const noLinkAlert = () => {
+            alert("There are no links yet.");
+        };
+        
         if (whatToDo == 1) {
             
             if (showLinks){
                     alert(`Here is the link list:\n${showLinks}`);
                 }   else {
-                    alert("There are no links yet.");
+                    noLinkAlert();
                 }
         } else if (whatToDo == 3) {
 			if (showLinks !== ""){
 				deleteNum = prompt(`Here are the links:\n${showLinks}\nEnter the number of the link to delete`);
-				if (deleteNum <= 0 || deleteNum > links.length){
-					//out of bounds
+				if (deleteNum <= 0 || deleteNum > links.length){//if out of bounds
 					alert("No link deleted. Enter a valid link number.");
 				}   else{
 					links.splice(Number(deleteNum - 1), 1);
 					alert("Link deleted.");
 				}
 			}    else {
-				alert("There are no links yet.");
+				noLinkAlert();
 			}
         }
     }
+
     switch(choice) {
         case "1": {
             listLinks(1);
@@ -67,10 +78,10 @@ while(choice !== "0"){
             links.push(new Link(title, url, author));
             break;
         }
-        case "3": {//delete not working, maybe convert to Number
+        case "3": {
             listLinks(3);
             break;
         }
     }
 }
-alert("See ya later!");
+alert("Winners never quit. Those who are done do.");
